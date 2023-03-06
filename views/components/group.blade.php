a826ce0c58c0c127978c19ce8f208caa12ee4834
Jyri Isomäki <isomjyri@gmail.com>
2023-03-06 15:47:47 +0200

    Add group label and description

--- views/components/group.blade.php
+++ views/components/group.blade.php
@@ -1,13 +1,27 @@
-<div class="space-y-4">
-    @foreach($fields as $field)
-        @php
-            $data = $this->findField($field->key);
+<div>
+    @if($label || $description)
+        <div class="mb-6">
+            @if($label)
+                <h2 class="text-lg leading-6 font-medium text-gray-900">{{ $label }}</h2>
+            @endif
 
-            if(!empty($field->fields)) {
-                $data = array_merge($data, ['fields' => $field->fields]);
-            }
-        @endphp
+            @if($description)
+                <p class="mt-1 text-sm text-gray-600">{{ $description }}</p>
+            @endif
+        </div>
+    @endif
 
-        {{ $field->render()->with($data) }}
-    @endforeach
+    <div class="space-y-4">
+        @foreach($fields as $field)
+            @php
+                $data = $this->findField($field->key);
+
+                if(!empty($field->fields)) {
+                    $data = array_merge($data, ['fields' => $field->fields]);
+                }
+            @endphp
+
+            {{ $field->render()->with($data) }}
+        @endforeach
+    </div>
 </div>
